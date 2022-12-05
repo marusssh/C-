4ce0e9c030c09f2635f9d6bfe102d2baecce9888
Marina <marinashulyateva@gmail.com>
2022-12-05 03:10:33 +0300

    Решили первую задачу

---
+++ Seminar2/Ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую 
+//цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+string number = Console.ReadLine();
+int startIndex = 1;
+int length = 1;
+
+String second_number = number.Substring(startIndex, length);
+
+Console.WriteLine(second_number);
