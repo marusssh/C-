7bb3644d00f1b58e1d16542de2ba74c6e3c19ecc
Marina <marinashulyateva@gmail.com>
2022-12-10 17:40:17 +0300

    Убрали лишнее

--- Seminar3/Ex03/Program.cs
+++ Seminar3/Ex03/Program.cs
@@ -3,25 +3,6 @@
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
 
-//int a;
-//int b;
-//int beg;  
-//int end=0;
-//Console.Write("Введите число: "); 
-
-//a = Convert.ToInt32(Console.ReadLine ());  
-          //beg=a;      
-          //while(a>0)      
-          //{      
-           //b=a%10;      
-           //end=(end*10)+b;      
-          // a=a/10;      
-      //    }      
-       //   if(beg==end)      
-     //      Console.Write("Число - полиндром");      
-    //      else      
-    //       Console.Write("Число не является полиндромом");    
-
 Console.Write("Введите число: "); 
 
 double n = Convert.ToDouble (Console.ReadLine());
